df9e9f8963dfb6543a671325573b6eca25817996
Rock Hudson <rockhudsonmillas@gmail.com>
2025-01-28 17:28:17 -0300

    Atualizando Painel responveis

--- Projeto Agora Cosmeticos/forms/Projeto Agora Cosmeticos/events/validateForm.js
+++ Projeto Agora Cosmeticos/forms/Projeto Agora Cosmeticos/events/validateForm.js
@@ -20,14 +20,14 @@ function validateForm(form){
     }
 
       //VALIDAÇÃO PAINEL REPONSAVEIS
-      var responsaveis = form.getChildrenIndexes(responsaveisTabela);
+      var responsaveis = form.getChildrenIndexes("responsaveisTabela");
       if (responsaveis.length == 0) {
           msg += "Nenhum responsável atribuido"
       }
 
     //VALIDAÇÃO CAMPOS PAINEL PONTO COMERCIAL
     if(form.getValue("segunda") != "on" &&  form.getValue("terca") != "on" && form.getValue("quarta") != "on" &&  form.getValue("quinta") != "on" &&  form.getValue("sexta") != "on" && form.getValue("sabado") != "on" &&  form.getValue("domingo") != "on" );
-    
+
     if(form.getValue("tipoPonto") == ""){
         msg += "Selecione o tipo de ponto!";
     }
