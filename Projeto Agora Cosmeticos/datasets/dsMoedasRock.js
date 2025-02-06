6780716e7c96e7cabb893759d5b05cf45e8ace7c
Rock Hudson <rockhudsonmillas@gmail.com>
2025-02-06 18:15:57 -0300

    Estruturando DataSet

---
+++ Projeto Agora Cosmeticos/datasets/dsMoedasRock.js
@@ -0,0 +1,27 @@
+function defineStructure() {
+
+}
+function onSync(lastSyncDate) {
+
+}
+function createDataset(fields, constraints, sortFields) {
+
+    var ds = DatasetBuilder.newDataset();
+
+    ds.addColumn("simbolo");
+    ds.addColumn("nomeMoeda");
+
+    ds.addRow(new Array("R$", "Real"));
+    ds.addRow(new Array("US$", "Dolar Americano"));
+    ds.addRow(new Array("U$", "Peso Uruguaio"));
+
+    return ds;
+
+
+}
+
+
+
+function onMobileSync(user) {
+
+}
